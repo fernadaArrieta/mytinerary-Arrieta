043947761f03ee470a793a33493adfdf83dd2d2a
FernadaArrieta <arrietafernanda6@gmail.com>
2022-02-18 12:49:10 -0300

    arreglando el renderizado

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import  ResponsiveAppBar  from './componentes/header';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import CarrouselApp from './componentes/carrousel'
-import Cities from './componentes/cities'
-import { Link as LinkRouter } from "react-router-dom"
-import Footer from './componentes/footer';
+import logo from "./logo.svg";
+import "./App.css";
+import ResponsiveAppBar from "./componentes/header";
+import CarrouselApp from "./componentes/carrousel";
+import Footer from "./componentes/footer";
 
 function App() {
   return (
-    
     <div className="App">
-      <BrowserRouter>
-      
-       <ResponsiveAppBar/>
-              
-       <div className='contenedor'>           
-      <img  className='imagen1'src={require('./ciudad de Mendoza/mendoza-capitaljpg.jpg')} alt="ciudad" />
-      <h1 className='isotipo'> "Find your perfect trip, designed by insider who Know and love their cities!"</h1>
+      <ResponsiveAppBar />
+
+      <div className="contenedor">
+        <img
+          className="imagen1"
+          src={require("./ciudad de Mendoza/mendoza-capitaljpg.jpg")}
+          alt="ciudad"/>
+        <h1 className="isotipo">         
+          "Find your perfect trip, designed by insider who Know and love their
+          cities!"
+        </h1>
       </div>
-        
-        <h2>Popular Mytineraries</h2>
-        <Routes>
-        
-        <Route path='/'element={<CarrouselApp/>}/>
-        {/* <Route path='*'element={<Cities/>}/>  */}
-        
-        </Routes>
-        {/* <Footer/> */}
-        </BrowserRouter>
-        
-        </div>
-       
-        
-    
-        
-    
+      <h2>Popular Mytineraries</h2>
+      <CarrouselApp />
+      <Footer/>
+    </div>
   );
 }
 
--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -1,15 +1,15 @@
-
+import "../styles/footer.css"
 
 function Footer(){
     return(
-        <div>
+        <div className="footerNav">
             <ul className="navFooter">
-            <li><a href="#">MyTinerary</a></li>
-            <li><a href="#">Cities</a></li>
-            <li><a href="#">Sección</a></li>
-            <li><a href="#">Sección</a></li>
+            <li>MyTinerary</li>
+            <li>Cities</li>
+            <li>Sección</li>
+            <li>Sección</li>
         </ul>
-        <div className="mapa"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53604.442668842894!2d-68.89474035485009!3d-32.89082618945539!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e093ec45179bf%3A0x205a78f6d20efa3a!2sMendoza%2C%20Capital%2C%20Mendoza!5e0!3m2!1ses!2sar!4v1645194273149!5m2!1ses!2sar" width="200" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53604.442668842894!2d-68.89474035485009!3d-32.89082618945539!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e093ec45179bf%3A0x205a78f6d20efa3a!2sMendoza%2C%20Capital%2C%20Mendoza!5e0!3m2!1ses!2sar!4v1645194273149!5m2!1ses!2sar" width="200" height="200"></iframe>
         </div>
     )
 }
