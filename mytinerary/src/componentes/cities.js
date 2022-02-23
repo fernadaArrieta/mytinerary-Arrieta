9f2be0fda6bd2362bd44819f0e3b6080e7ee2b8c
FernadaArrieta <arrietafernanda6@gmail.com>
2022-02-23 17:05:36 -0300

    card estaticas y favicom

--- mytinerary/src/componentes/cities.js
+++ mytinerary/src/componentes/cities.js
@@ -1,12 +1,101 @@
 
 
+ import * as React from 'react';
+ import Card from '@mui/material/Card';
+ import CardActions from '@mui/material/CardActions';
+ import CardContent from '@mui/material/CardContent';
+ import CardMedia from '@mui/material/CardMedia';
+ import Button from '@mui/material/Button';
+ import Typography from '@mui/material/Typography';
  
+ export default function Cities() {
+   return (
+       <div className='card'>
+     <Card sx={{ maxWidth: 345, margin:3 }}>
+       <CardMedia
+         component="img"
+         height="140"
+         image="https://depor.com/resizer/EH-t0H8HZsYrjUWI326XVhlfK7M=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BZK5BQF5VRCKDAID6A54R6LBP4.jpg"
+         alt="foto-ciudad"
+       />
+       <CardContent sx={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center" }}>
+         <Typography gutterBottom variant="h5" component="div">
+           Rio de Janeiro
+         </Typography>
+         <Typography variant="body2" color="text.secondary">
+          Aca irian detalles
 
- export default function Cities(){
+         </Typography>
+       </CardContent>
+       <CardActions sx={{display:"flex", alignItems:"center", justifyContent:"center" }}>
+         
+         <Button size="small" >Learn More</Button>
+       </CardActions>
+     </Card>
+     <Card sx={{ maxWidth: 345, margin:3 }}>
+       <CardMedia
+         component="img"
+         height="140"
+         image="https://depor.com/resizer/EH-t0H8HZsYrjUWI326XVhlfK7M=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BZK5BQF5VRCKDAID6A54R6LBP4.jpg"
+         alt="foto-ciudad"
+       />
+       <CardContent sx={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center" }}>
+         <Typography gutterBottom variant="h5" component="div">
+           Rio de Janeiro
+         </Typography>
+         <Typography variant="body2" color="text.secondary">
+          Aca irian detalles
 
-    return( 
-        <div className= 'cont-cities'>
-        <h1 className='pagCities'>"Page Under Construction"</h1>
-        </div>
-    )
-}
+         </Typography>
+       </CardContent>
+       <CardActions sx={{display:"flex", alignItems:"center", justifyContent:"center" }}>
+         
+         <Button size="small" >Learn More</Button>
+       </CardActions>
+     </Card>
+     <Card sx={{ maxWidth: 345, margin:3 }}>
+       <CardMedia
+         component="img"
+         height="140"
+         image="https://depor.com/resizer/EH-t0H8HZsYrjUWI326XVhlfK7M=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BZK5BQF5VRCKDAID6A54R6LBP4.jpg"
+         alt="foto-ciudad"
+       />
+       <CardContent sx={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center" }}>
+         <Typography gutterBottom variant="h5" component="div">
+           Rio de Janeiro
+         </Typography>
+         <Typography variant="body2" color="text.secondary">
+          Aca irian detalles
+
+         </Typography>
+       </CardContent>
+       <CardActions sx={{display:"flex", alignItems:"center", justifyContent:"center" }}>
+         
+         <Button size="small" >Learn More</Button>
+       </CardActions>
+     </Card>
+     <Card sx={{ maxWidth: 345, margin:3 }}>
+       <CardMedia
+         component="img"
+         height="140"
+         image="https://depor.com/resizer/EH-t0H8HZsYrjUWI326XVhlfK7M=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BZK5BQF5VRCKDAID6A54R6LBP4.jpg"
+         alt="foto-ciudad"
+       />
+       <CardContent sx={{display:"flex",flexDirection:"column", alignItems:"center", justifyContent:"center" }}>
+         <Typography gutterBottom variant="h5" component="div">
+           Rio de Janeiro
+         </Typography>
+         <Typography variant="body2" color="text.secondary">
+          Aca irian detalles
+
+         </Typography>
+       </CardContent>
+       <CardActions sx={{display:"flex", alignItems:"center", justifyContent:"center" }}>
+         
+         <Button size="small" >Learn More</Button>
+       </CardActions>
+     </Card>
+     </div>
+   );
+ }
+
