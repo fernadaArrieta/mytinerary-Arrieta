62b14f282ef81143a2365e744310ed0b4f873250
FernadaArrieta <arrietafernanda6@gmail.com>
2022-02-24 22:34:53 -0300

    intale mongo y y cree database

--- mytinerary/backend/server.js
+++ mytinerary/backend/server.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express= require('express')
+require('./config/database')
 const PORT= 4000
 
 const app= express()
 
-app.listen(PORT,() =>console.log ('Server ready on PORT'+PORT))
+app.listen(PORT,()=>console.log ('Server ready on PORT'+PORT))
+
