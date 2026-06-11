47a43b217d740aa243a5996e2b6f00c8ab7ecf56
Fernanda Arrieta <99426887+fernadaArrieta@users.noreply.github.com>
2026-06-11 07:40:10 -0300

    sin cambios

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
-require('dotenv').config()
-const cors = require('cors')
-const passport = require("passport")
-const express= require('express')
-require('./config/database')
+require("dotenv").config();
+const cors = require("cors");
+const passport = require("passport");
+const express = require("express");
+require("./config/database");
 
-const Router = require('./routes/routes')
-const RouterTinerary = require('./routes/routestinerary')
-const RoutesUsers = require('./routes/routesusers')
-const PORT= 4000
+const Router = require("./routes/routes");
+const RouterTinerary = require("./routes/routestinerary");
+const RoutesUsers = require("./routes/routesusers");
+const PORT = 4000;
 
-
-const app= express()
+const app = express();
 
 //middlewares
-app.use(cors())
-app.use(express.json())
-app.use('/api', Router)
-app.use('/api/v1', RouterTinerary)
-app.use('/api',RoutesUsers)
+app.use(cors());
+app.use(express.json());
+app.use("/api", Router);
+app.use("/api/v1", RouterTinerary);
+app.use("/api", RoutesUsers);
 
-app.use(passport.initialize())
+app.use(passport.initialize());
 
-app.listen(PORT,()=>console.log ('Server ready on PORT'+PORT))
-
+app.listen(PORT, () => console.log("Server ready on PORT" + PORT));
